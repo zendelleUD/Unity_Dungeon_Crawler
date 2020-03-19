f9bf321dfd3c7a82349b9f476f1fcc38a9bff40c
Zendelle Badiang <Zendelle@udel.edu>
2020-03-18 23:36:09 -0400

    Added animations and Blend Trees

---
+++ Assets/Scripts/Camera_Follow.cs
@@ -0,0 +1,13 @@
+﻿
+using UnityEngine;
+
+public class Camera_Follow : MonoBehaviour
+{
+
+    public Transform player;
+
+    void FixedUpdate(){
+        transform.position = new Vector3(player.position.x,player.position.y,transform.position.z); 
+    }
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+
+    public float Movspeed = 2f;
+    public Animator animator;
+    public Rigidbody2D rb;
+    Vector2 movement;
+    public float Horizontal_Direction;
+    public float Vertical_Direciton;
+        
+    // Update is called once per frame
+    void Update()
+    {
+        Horizontal_Direction =Input.GetAxisRaw("Horizontal");
+        Vertical_Direciton = Input.GetAxisRaw("Vertical");
+        animator.SetFloat("Horizontal",Horizontal_Direction);
+        animator.SetFloat("Vertical", Vertical_Direciton);
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate(){
+        rb.MovePosition(rb.position + movement * Movspeed * Time.fixedDeltaTime);
+    }
+}
