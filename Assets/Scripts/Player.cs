9fcac0067bd0e39ce98a8ce87fd201c35b4974b6
Zendelle Badiang <Zendelle@udel.edu>
2020-03-19 10:56:57 -0400

    Changed Player sprite and fixed missing keyframes in animations, Changes to Player_Script: Added UpdateAnimator Method to handle animations and Move_Player for cleanliness

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,21 +8,54 @@ public class Player : MonoBehaviour
     public Animator animator;
     public Rigidbody2D rb;
     Vector2 movement;
+
+    //Meantsfor debugging in Unity Ide
     public float Horizontal_Direction;
     public float Vertical_Direciton;
+
+    /*
+    UpdateAnimator
+        x_axis  float valued between -1 & 1
+        y_axis  float valued between -1 & 1
+    
+    Description:
+        Takes in Input.GetAxisRaw() for x and y directions and updates
+        object Animator's Horiztonal and Vertical Attributes for blend tree
+    */
+     void updateAnimator(float x_axis,float y_axis){
+        Horizontal_Direction = x_axis;
+        Vertical_Direciton = y_axis;
         
-    // Update is called once per frame
-    void Update()
-    {
-        Horizontal_Direction =Input.GetAxisRaw("Horizontal");
-        Vertical_Direciton = Input.GetAxisRaw("Vertical");
+        if(x_axis <= -.01){
+            // animator.SetBool("Flip_x",true);
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+        }else if(x_axis >= .01){
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+            // animator.SetBool("Flip_x", false);
+        }
+
         animator.SetFloat("Horizontal",Horizontal_Direction);
         animator.SetFloat("Vertical", Vertical_Direciton);
+     }   
+
+    /*
+    Handles Player Movements
+    */
+     void Move_Player(){
+        rb.MovePosition(rb.position + movement * Movspeed * Time.fixedDeltaTime);
+     }
+    // Update is called once per frame
+    // Variable rate of frames, not good for physics
+    void Update()
+    {
+        updateAnimator(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"));
+        
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
     }
 
+    //Called 50 times a second, Best for physics
     void FixedUpdate(){
-        rb.MovePosition(rb.position + movement * Movspeed * Time.fixedDeltaTime);
+       Move_Player();
     }
 }
