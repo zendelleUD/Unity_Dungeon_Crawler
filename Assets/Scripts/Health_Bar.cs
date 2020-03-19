dc6577fcaa4353ccb343df619703780cc6dd82bf
Zendelle Badiang <Zendelle@udel.edu>
2020-03-19 15:02:43 -0400

    Health Bar created with script

---
+++ Assets/Scripts/Health_Bar.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Health_Bar : MonoBehaviour
+{
+    
+   public Slider slider;
+
+   public void setHealth(int health){
+       slider.value = health;
+   }
+
+   public void setMaxValue(int health){
+       slider.maxValue = health;
+       setHealth(health);
+   }
+   
+}
