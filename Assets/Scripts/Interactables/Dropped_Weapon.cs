9df07e2e2d50975fd2c23ff0b1f3a0c9b2a76a00
Zendelle Badiang <Zendelle@udel.edu>
2020-03-25 18:41:09 -0400

    added dropped weapon Destroy interaction

---
+++ Assets/Scripts/Interactables/Dropped_Weapon.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dropped_Weapon : MonoBehaviour
+{
+   
+    public void DeleteDroppedItem(){
+        Destroy(gameObject);
+    }
+
+
+
+}
---
+++ Assets/Scripts/Interactables/Interactable.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Interactable: MonoBehaviour
+{
+
+    public bool isInRange;
+    public KeyCode interactKey;
+    public UnityEvent interactAction;
+
+    
+    void Update(){
+        if(isInRange){ //Checks if the Player is withing the Object Collider with onTriggerEnter2d()
+            if(Input.GetKeyDown(interactKey)){ //Checks if Key input is set to right key.
+                 interactAction.Invoke(); //Fire Events
+            }
+        }
+    }
+
+    /* Is called whenever a collision is detected entering the object the script is attached to. */
+    private void OnTriggerEnter2D(Collider2D collision){
+        if(collision.gameObject.CompareTag("Player")){
+            isInRange = true;
+            Debug.Log("Player Entered");
+        }
+        
+    }
+
+    /* Is called whenever a collision is detected exeting the object the script is attached to. */
+    private void OnTriggerExit2D(Collider2D collision){
+        if(collision.gameObject.CompareTag("Player")){
+            isInRange = false;
+            Debug.Log("Player Left");
+        }
+    }
+
+}
